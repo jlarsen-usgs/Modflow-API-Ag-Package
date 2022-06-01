67fb8b5cb36a15578f1bf3c8f50f1ccc72c20775
Joshua Larsen <jlarsen@usgs.gov>
2022-05-31 17:32:28 -0700

    added common.py to autotest directory

---
+++ autotest/common.py
@@ -0,0 +1,32 @@
+import os
+from math import log10, floor
+
+
+def round_to_n(x, n):
+    if x == 0:
+        return 0
+    t = round(x, -int(floor(log10(abs(x))) - (n - 1)))
+    return t
+
+
+def mf6_dev_no_final_check(model_ws, fname):
+    contents = []
+    with open(os.path.join(model_ws, fname)) as foo:
+        for line in foo:
+            if "options" in line.lower():
+                contents.append(line)
+                contents.append("  DEV_NO_FINAL_CHECK\n")
+            else:
+                contents.append(line)
+
+    with open(os.path.join(model_ws, fname), "w") as foo:
+        for line in contents:
+            foo.write(line)
+
+
+def dll_loc():
+    return os.path.join("..", "modflow-bmi", "libmf6.dll")
+
+
+def nwt_output_path():
+    return os.path.join("..", "data", "nwt_etdemand_test_problems")
