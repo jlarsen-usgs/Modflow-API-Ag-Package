dc4e795eb7b1392573a5654d7bbbf25c7a32bd1a
Joshua Larsen <jlarsen@usgs.gov>
2022-07-15 14:26:01 -0700

    move agmvr_test_model to examples

--- examples/flopy_agmvr_test_model/compare_ag_mvr_conjunctive.py
+++ examples/flopy_agmvr_test_model/compare_ag_mvr_conjunctive.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from scipy.stats import linregress
 sws = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.join(sws, "..", "mf6api_agmvr"))
+sys.path.append(os.path.join(sws, "..", "..", "mf6api_agmvr"))
 from mf6_agmvr import ModflowAgmvr
 mpl.use("TKAgg")
 
@@ -24,7 +24,7 @@ def round_to_n(x, n):
 
 
 def build_mf6(name, headtol=None, fluxtol=None):
-    sim_ws = os.path.join(sws, "..", "data", "mf6_agmvr_test_problems")
+    sim_ws = os.path.join(sws, "..", "..", "data", "mf6_agmvr_test_problems")
     sim = flopy.mf6.MFSimulation(name, sim_ws=sim_ws)
 
     perlen = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
@@ -150,7 +150,7 @@ def build_mf6(name, headtol=None, fluxtol=None):
                                   mover=True,
                                   observations=obs_dict)
 
-    cimis_data = os.path.join("..", "data", "davis_monthly_ppt_eto.txt")
+    cimis_data = os.path.join("..", "..", "data", "davis_monthly_ppt_eto.txt")
     df = pd.read_csv(cimis_data)
 
     # build a UZF package
@@ -364,18 +364,16 @@ def compare_model_output(nwt, mf6, model):
 if __name__ == "__main__":
     load_existing = False
     run_model = True
-    dll = os.path.join("..", "modflow-bmi", "libmf6.dll")
-    mf6_ws = os.path.join(sws, "..", "data", "mf6_agmvr_test_problems")
-    nwt_ws = os.path.join(sws, "..", "data", "nwt_etdemand_gwet_test_problems")
+    dll = os.path.join("..", "..", "modflow-bmi", "libmf6.dll")
+    mf6_ws = os.path.join(sws, "..", "..", "data", "mf6_agmvr_test_problems")
+    nwt_ws = os.path.join(sws, "..", "..", "data", "nwt_etdemand_gwet_test_problems")
     if run_model:
         if not load_existing:
             sim, gwf = build_mf6("etdemand_sup")
         else:
-            sim = flopy.mf6.MFSimulation.load(
-                sim_ws=os.path.join(sws, "..", "data", "mf6_agmvr_test_problems")
-            )
+            sim = flopy.mf6.MFSimulation.load(sim_ws=mf6_ws)
+
         mfag = ModflowAgmvr(sim, ag_type="etdemand", mvr_name="agmvr")
         mfag.run_model(dll)
-    else:
-        gwf = None
+
     compare_model_output(nwt_ws, mf6_ws, "etdemand_sup")
