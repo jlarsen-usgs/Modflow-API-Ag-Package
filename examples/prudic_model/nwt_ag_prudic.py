05dcf1b40877a9a8831017043b39383682990b90
Joshua Larsen <jlarsen@usgs.gov>
2022-07-19 14:04:03 -0700

    initial version of prudic models created

--- examples/prudic_model/nwt_ag_prudic.py
+++ examples/prudic_model/nwt_ag_prudic.py
@@ -172,8 +172,7 @@ def build_model(name, model_ws):
     nss = len(reach_data.seg.unique())
     const = 86400
     dleak = 1e-06
-    isfropt = 2
-    reachinput = True
+    isfropt = 0
     nstrail = 15
     isuzn = 1
     nsfrsets = 30
@@ -185,23 +184,122 @@ def build_model(name, model_ws):
         new_rec = (node, rec[0], rec[1], rec[2], rec[3], rec[4], rec[5], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
         recarray[ix] = new_rec
     reach_data = recarray
-    # todo: segment_data SFR
 
     seg_data = flopy.modflow.ModflowSfr2.get_empty_segment_data(nss)
     records =[
-        (0, 1, 1, 0, 0, 25.0, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1093.048, 12, 0, 0, 0, 0, 0, 0.00003, 3.0, 1077.408, 12, 0, 0, 0, 0, 0),
-        (1, 1, 5, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1071.934, 12, 0, 0, 0, 0, 0, 0.00003, 3.0, 1052.359, 12, 0, 0, 0, 0, 0),
-        (2, 1, 4, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1073.636, 10, 0, 0, 0, 0, 0, 0.00003, 3.0, 1060.545, 10, 0, 0, 0, 0, 0),
-        (3, 1, 4, 0, 0, 10.0, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1077.727, 10, 0, 0, 0, 0, 0, 0.00003, 3.0, 1063.182, 10, 0, 0, 0, 0, 0),
-        (4, 1, 5, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1058.000, 15, 0, 0, 0, 0, 0, 0.00003, 3.0, 1046.250, 15, 0, 0, 0, 0, 0),
-        (5, 1, 7, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1042.727, 12, 0, 0, 0, 0, 0, 0.00003, 3.0, 1025.909, 12, 0, 0, 0, 0, 0),
-        (6, 1, 7, 0, 0, 150., 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1037.581, 55, 0, 0, 0, 0, 0, 0.00003, 3.0, 1027.419, 55, 0, 0, 0, 0, 0),
-        (7, 1, 0, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1021.875, 40, 0, 0, 0, 0, 0, 0.00003, 3.0, 991.8750, 40, 0, 0, 0, 0, 0),
-        (8, 1, 0, 1, 0, 100., 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1051.000, 4., 0, 0, 0, 0, 0, 0.00003, 3.0, 1050.000, 4., 0, 0, 0, 0, 0)
+        (1, 1, 2, 0, 0, 0, 25.0, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1093.048, 12, 0, 0, 0, 0, 0, 0.00003, 3.0, 1077.408, 12, 0, 0, 0, 0, 0),
+        (2, 1, 6, 0, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1071.934, 12, 0, 0, 0, 0, 0, 0.00003, 3.0, 1052.359, 12, 0, 0, 0, 0, 0),
+        (3, 1, 5, 0, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1073.636, 10, 0, 0, 0, 0, 0, 0.00003, 3.0, 1060.545, 10, 0, 0, 0, 0, 0),
+        (4, 1, 5, 0, 0, 0, 10.0, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1077.727, 10, 0, 0, 0, 0, 0, 0.00003, 3.0, 1063.182, 10, 0, 0, 0, 0, 0),
+        (5, 1, 6, 0, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1058.000, 15, 0, 0, 0, 0, 0, 0.00003, 3.0, 1046.250, 15, 0, 0, 0, 0, 0),
+        (6, 1, 8, 0, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1042.727, 12, 0, 0, 0, 0, 0, 0.00003, 3.0, 1025.909, 12, 0, 0, 0, 0, 0),
+        (7, 1, 8, 0, 0, 0, 150., 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1037.581, 55, 0, 0, 0, 0, 0, 0.00003, 3.0, 1027.419, 55, 0, 0, 0, 0, 0),
+        (8, 1, 0, 0, 0, 0, 0.00, 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1021.875, 40, 0, 0, 0, 0, 0, 0.00003, 3.0, 991.8750, 40, 0, 0, 0, 0, 0),
+        (9, 1, 0, 2, 0, 0, 100., 0, 0, 0, 0.03, 0, 0, 0, 0, 0, 0.00003, 3.0, 1051.000, 4., 0, 0, 0, 0, 0, 0.00003, 3.0, 1050.000, 4., 0, 0, 0, 0, 0)
     ]
 
-    print('break')
-    # todo: AG package
+    stress_period_data = {}
+    for ix in range(nss):
+        seg_data[ix] = records[ix]
+
+    for per in range(nper):
+        stress_period_data[per] = seg_data.copy()
+
+    sfr = flopy.modflow.ModflowSfr2(
+        ml,
+        nstrm=nstrm,
+        nss=nss,
+        const=const,
+        dleak=dleak,
+        ipakcb=102,
+        isfropt=isfropt,
+        nstrail=nstrail,
+        isuzn=isuzn,
+        nsfrsets=nsfrsets,
+        reach_data=reach_data,
+        segment_data=stress_period_data
+    )
+
+    # create AG package
+    optstr = "ETDEMAND " \
+             "IRRIGATION_DIVERSION 1 6 " \
+             "SUPPLEMENTAL_WELL 6 1 " \
+             "IRRIGATION_WELL 6 1 " \
+             "maxwells 6 " \
+             "timeseries_diversion"
+
+    options = flopy.utils.OptionBlock(optstr.lower(), flopy.modflow.ModflowAg)
+
+    time_series = flopy.modflow.ModflowAg.get_empty(1, block="time series")
+    time_series[0] = ("diversion", 9, 107)
+
+    well_list = flopy.modflow.ModflowAg.get_empty(6, block="well")
+    cnt = 0
+    for i in range(5, 8):
+        for j in range(3, 5):
+            well_list[cnt] = (0, i, j, -100.)
+            cnt += 1
+
+    # create empty records for AG package
+    nirr = 0
+    wel = flopy.modflow.ModflowAg.get_empty(nirr, block="irrwell")
+    sup = flopy.modflow.ModflowAg.get_empty(nirr, block="supwell")
+    div = flopy.modflow.ModflowAg.get_empty(nirr, block="irrdiversion")
+
+    irrdiversion = {}
+    irrwell = {}
+    supwell = {}
+    for per in range(4):
+        irrdiversion[per] = div.copy()
+        irrwell[per] = wel.copy()
+        supwell[per] = sup.copy()
+
+    # assemble ag package stress period data
+    nirr = 6
+    maxells = 1
+    wel = flopy.modflow.ModflowAg.get_empty(nirr, maxells, "irrwell")
+    sup = flopy.modflow.ModflowAg.get_empty(nirr, maxells, "supwell")
+    cnt = 0
+    for i in range(5, 8):
+        for j in range(3, 5):
+            wel[cnt] = (cnt, 1, 0., 0., i, j, 0., 1.)
+            sup[cnt] = (cnt, 1, 9, 1., 1.)
+
+    nirr = 1
+    maxells = 6
+    div = flopy.modflow.ModflowAg.get_empty(nirr, maxells, "irrdiversion")
+    fr = 1./6.
+    div[0] = (
+        9, 6, 0, 0,
+        5, 3, 0, fr,
+        5, 4, 0, fr,
+        6, 3, 0, fr,
+        6, 4, 0, fr,
+        7, 3, 0, fr,
+        8, 4, 0, fr
+    )
+
+    for per in range(4, 49):
+        irrwell[per] = wel.copy()
+        irrdiversion[per] = div.copy()
+        supwell[per] = sup.copy()
+
+    ag = flopy.modflow.ModflowAg(
+        ml,
+        options=options,
+        time_series=time_series,
+        well_list=well_list,
+        irrdiversion=irrdiversion,
+        irrwell=irrwell,
+        supwell=supwell
+    )
+
+    ml.write_input()
+    nam = os.path.join(model_ws, f"{name}.nam")
+
+    with open(nam, "a") as foo:
+        foo.write(f"DATA             107  {name}.diversion9.txt")
+
 
 if __name__ == "__main__":
     model_ws = os.path.join("..", "..", "data", "nwt_prudic_ag")
